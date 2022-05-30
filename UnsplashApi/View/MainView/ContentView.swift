f9189a21f87a9572f33a5b15d9051a766bc6ca76
2007riot <2007riot@gmail.com>
2022-05-30 21:27:54 +0200

    Still thinking about search text field

--- UnsplashApi/View/MainView/ContentView.swift
+++ UnsplashApi/View/MainView/ContentView.swift
@@ -1,35 +1,35 @@
-////
-////  ContentView.swift
-////  UnsplashApi
-////
-////  Created by Galina Aleksandrova on 26/05/22.
-////
 //
-//import SwiftUI
+//  ContentView.swift
+//  UnsplashApi
 //
-//struct ContentView: View {
-//    @StateObject var apiResultFetcher = SearchResultFetcher()
-//    var body: some View {
-//        
-//        Group {
-//        if apiResultFetcher.isLoading {
-//            ProgressView()
-//        } else if apiResultFetcher.errorMessage != nil {
-//            ErrorMainView(apiResultFetcher: apiResultFetcher)
-//        } else {
-//            ResultListView(photoResults: apiResultFetcher.apiResults)
-//        }
-//    }
-//        .onAppear() {
-//            apiResultFetcher.fetch()
-//        }
-//        
-//    }
-//    
-//}
+//  Created by Galina Aleksandrova on 26/05/22.
 //
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var apiResultFetcher = SearchResultFetcher()
+    var body: some View {
+        
+        Group {
+        if apiResultFetcher.isLoading {
+            ProgressView()
+        } else if apiResultFetcher.errorMessage != nil {
+            ErrorMainView(apiResultFetcher: apiResultFetcher)
+        } else {
+            ResultListView(apiResults: apiResultFetcher.apiResults)
+        }
+    }
+        .onAppear() {
+            apiResultFetcher.fetch()
+        }
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- UnsplashApi/View/ResultListView.swift
+++ UnsplashApi/View/ResultListView.swift
@@ -9,26 +9,11 @@ import SwiftUI
 
 struct ResultListView: View {
    
-    @StateObject var apiResultFetcher = SearchResultFetcher()
+    let apiResults : [APIResult]
     var body: some View {
         NavigationView {
-            VStack {
-                TextField("Search images", text: $apiResultFetcher.searchText)
-                    .onSubmit {
-                        apiResultFetcher.fetch()
-                    }
-                    .padding()
-                    .textFieldStyle(.roundedBorder)
-                Group {
-                    if apiResultFetcher.isLoading {
-                        ProgressView()
-                        Spacer ()
-                    } else if apiResultFetcher.errorMessage != nil {
-                        ErrorMainView(apiResultFetcher: apiResultFetcher)
-                        Spacer()
-                    } else {
                         List {
-                            ForEach(apiResultFetcher.apiResults, id: \.self) { apiResult in
+                            ForEach(apiResults, id: \.self) { apiResult in
                                 
                                 NavigationLink {
                                     ImageDetailView(apiResult: apiResult)
@@ -40,22 +25,19 @@ struct ResultListView: View {
                         .listStyle(.plain)
                         .navigationTitle("Unsplash")
                     }
-                }
-
-                    
-            }
-            }
                 .onAppear() {
+                    
             //so we safe network data usage, but increase our phone memory with images 
             print("cache size: \(URLCache.shared.memoryCapacity / 1024) KB")
             //increase cache size to half of gigabyte
             URLCache.shared.memoryCapacity = 1024 * 1024 * 512
         }
+                
     }
 }
 
 struct ResultListView_Previews: PreviewProvider {
     static var previews: some View {
-        ResultListView()
+        ResultListView(apiResults: [APIResult.example1()])
     }
 }
